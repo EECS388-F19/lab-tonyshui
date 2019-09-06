606abb874e47300fd02a90ee65ae4507d9e09cc8
tonyshui <tonyshui@umich.edu>
2019-09-06 16:20:00 -0400

    lab 0 python

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+print("lab 0")
+print('Anthony Shui')
+x = random.randrange(0, 100)
+y = random.randrange(0, 100)
+print(x)
+print(y)
+print("Sum = ", x+y)
+print("Average = ", (x+y)//2)
+
+
+
+
